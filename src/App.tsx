0cc50027d4797260af0562ff074e19d152d9c325
TheDiabolicalKid <marine.opsommer@gmail.com>
2026-01-07 18:13:19 +0100

    add pokemon in my list with their own picture

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import "./App.css";
-import PokemonCard from './components/PokemonCard';
+import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "ivysaur",
+    imgSrc: 
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+  },
+  {
+    name: "mew",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+  },
+  
+
 ];
-
-
 
 function App() {
   return (
-   <div>
-    <PokemonCard pokemon={pokemonList[0]}/>
-   </div>
+    <div>
+      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[1]} />
+      <PokemonCard pokemon={pokemonList[2]} />
+    </div>
   );
 }
 
