8f691420a14d96f09d2c0deb6a02bf5125bd35eb
TheDiabolicalKid <marine.opsommer@gmail.com>
2026-01-08 12:49:32 +0100

    remove pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,8 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: "ivysaur",
-    imgSrc: 
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
-  },
-  {
     name: "mew",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+   
   },
   
 
@@ -26,7 +20,7 @@ function App() {
     <div>
       <PokemonCard pokemon={pokemonList[0]} />
       <PokemonCard pokemon={pokemonList[1]} />
-      <PokemonCard pokemon={pokemonList[2]} />
+      
     </div>
   );
 }
